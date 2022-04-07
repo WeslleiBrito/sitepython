b70c4e0e0e2d7fd09334c480a8a9e61531c4b905
WeslleiBrito <wesllei.wbs@gmail.com>
2022-04-07 17:20:19 -0300

    Lançamento

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+
+
+class Jogo:
+
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+jogo1 = Jogo('Free Fire', 'Battle Royale', 'Moble')
+jogo2 = Jogo('Call of Duty', 'Tiro em primeira pessoa', 'PC')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista_jogos = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+
+    return redirect('/')
+
+
+app.run(debug=True)
