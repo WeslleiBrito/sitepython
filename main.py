f6021fd58510624f106c3ca6fe869ae6d49b9c47
WeslleiBrito <wesllei.wbs@gmail.com>
2022-04-08 14:03:07 -0300

    AUTOMATIZANDO

--- main.py
+++ main.py
@@ -39,4 +39,17 @@ def criar():
     return redirect('/')
 
 
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+
 app.run(debug=True)
